2f9794cb11e141c1fbeea34dfca4274cdd7247d1
mayn <18616790184@163.com>
2020-04-18 18:52:08 +0800

    登录功能的实现

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,29 +1,79 @@
 <template>
   <div class="login">
-    <el-form ref="form" :model="form" label-width="80px">
-      <img src="../assets/images/avatar.jpg">
-      <el-form-item label="用户名">
-        <el-input v-model="form.usesrname"></el-input>
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      <img src="../assets/images/avatar.jpg" />
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
       </el-form-item>
-       <el-form-item label="密码">
-        <el-input v-model="form.password"></el-input>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="form.password" placeholder="请输入密码" type="password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">登录</el-button>
-        <el-button class="reset">重置</el-button>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button class="reset" @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
   data () {
     return {
       form: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.form.validate(valid => {
+        console.log('结果是', valid)
+        if (valid) {
+          // 校验成功
+          axios({
+            url: 'http://localhost:8888/api/private/v1/login',
+            method: 'post',
+            data: this.form
+          }).then(res => {
+            console.log(res.data)
+            if (res.data.meta.status === 200) {
+              this.$message({
+                message: '恭喜你,登录成功',
+                type: 'success'
+              })
+              // 存储token
+              localStorage.setItem('token', res.data.data.token)
+              // 跳转到Home组件中
+              this.$router.push('/home')
+              console.log(this.$router)
+            } else {
+              console.log('登录失败')
+              this.$message.error('登录失败')
+            }
+          })
+        } else {
+          // 校验失败
+          return false
+        }
+      })
+    },
+    reset () {
+      this.$refs.form.resetFields()
     }
   }
 }
@@ -31,28 +81,28 @@ export default {
 
 <style scoped lang="less">
 .login {
- width: 100%;
- height: 100%;
- background-color: #2d434c;
- overflow: hidden;
- .el-form {
-   width: 400px;
-   background-color: #fff;
-   padding: 75px 40px 15px 40px;
-   margin: 200px auto;
-   border-radius: 20px;
-   position: relative;
-   img {
-     position: absolute;
-     top: -70px;
-     left: 50%;
-     transform: translateX(-50%);
-     border-radius: 50%;
-     border: 10px solid #fff;
-   }
- }
- .reset {
-   margin-left: 120px;
- }
+  width: 100%;
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+  .el-form {
+    width: 400px;
+    background-color: #fff;
+    padding: 75px 40px 15px 40px;
+    margin: 200px auto;
+    border-radius: 20px;
+    position: relative;
+    img {
+      position: absolute;
+      top: -70px;
+      left: 50%;
+      transform: translateX(-50%);
+      border-radius: 50%;
+      border: 10px solid #fff;
+    }
+  }
+  .reset {
+    margin-left: 120px;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
-
+import Home from '@/components/Home'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
-    { path: '/', component: Login }
+
+    {
+      path: '/',
+      component: Login
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home
+    }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.path === '/login' || token) {
+    next()
+  } else {
+    next('login')
+  }
+})
+
+// 导出路由
+export default router
