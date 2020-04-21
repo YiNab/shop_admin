4cc65988099c8abd291405cdd731619a3668ee0f
mayn <18616790184@163.com>
2020-04-21 16:00:01 +0800

    角色列表中,分配权限功能实现

--- src/components/Roles.vue
+++ src/components/Roles.vue
@@ -54,21 +54,55 @@
         <template slot-scope="scope">
           <el-button plain type="primary" size="small" icon="el-icon-edit"></el-button>
           <el-button plain type="danger" size="small" icon="el-icon-delete"></el-button>
-          <el-button plain type="success" size="small" icon="el-icon-check">分配权限</el-button>
+          <el-button
+            plain
+            type="success"
+            size="small"
+            icon="el-icon-check"
+            @click="showAssignRight(scope.row)"
+          >分配权限</el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 表格展示 结束-->
+    <!-- 分配权限弹出框 开始 -->
+    <el-dialog title="分配权限" :visible.sync="assignDialogVisible" width="40%">
+      <!-- 权限的树形结构 数组 队列 栈 tree -->
+      <el-tree
+        ref="tree"
+        :data="rightList"
+        :props="defaultProps"
+        show-checkbox
+        default-expand-all
+        node-key="id"
+      ></el-tree>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="assignDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="assignRight">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 分配权限弹出框 结束 -->
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      roleList: []
+      // 角色数据
+      roleList: [],
+      // 权限数据
+      rightList: [],
+      // 分配权限弹出框
+      assignDialogVisible: false,
+      defaultProps: {
+        children: 'children',
+        label: 'authName'
+      },
+      roleId: ''
     }
   },
   methods: {
+    // 获取角色数据
     async getRolesList () {
       let res = await this.axios({
         method: 'get',
@@ -78,9 +112,18 @@ export default {
       let { meta: { status }, data } = res
       if (status === 200) {
         this.roleList = data
-        // console.log('查询成功', res)
+        console.log('查询成功', res)
       }
     },
+    // 获取列表数据
+    async getRightList () {
+      let res = await this.axios.get('rights/tree')
+      let { meta: { status }, data } = res
+      if (status === 200) {
+        this.rightList = data
+      }
+    },
+    // 删除权限数据
     async deletRight (role, rightId) {
       console.log(role, rightId)
       let res = await this.axios({
@@ -92,10 +135,49 @@ export default {
         role.children = data
         this.$message.success('删除成功')
       }
+    },
+    // 显示分配权限对话框
+    showAssignRight (role) {
+      this.roleId = role.id
+      this.getRightList()
+      this.assignDialogVisible = true
+      this.$nextTick(() => {
+        let temp = []
+        role.children.forEach(l1 => {
+          l1.children.forEach(l2 => {
+            l2.children.forEach(l3 => {
+              // 把3级的id存起来
+              temp.push(l3.id)
+            })
+          })
+        })
+        // 设置setCheckedKeys目前勾选的节点
+        this.$refs.tree.setCheckedKeys(temp)
+      })
+    },
+    // 分配权限
+    async assignRight () {
+      // 获取到所有选中的id值
+      let checked = this.$refs.tree.getCheckedKeys()
+      // 获取的所有半选中的id
+      let halfChecked = this.$refs.tree.getHalfCheckedKeys()
+      // console.log(checked, halfChecked)
+      // ... 扩展运算符,展开一个数组或者对象
+      let all = [...checked, ...halfChecked]
+      let res = await this.axios.post(`roles/${this.roleId}/rights`, {
+        rids: all.join()
+      })
+      let { meta: { status } } = res
+      if (status === 200) {
+        this.assignDialogVisible = false
+        // 重新渲染
+        this.getRolesList()
+      }
     }
   },
   created () {
     this.getRolesList()
+    this.getRightList()
   }
 }
 </script>
